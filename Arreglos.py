a8f9005e46b59181ef551919cf5406cf9041161b
leo028-SC <leonardo.sc274@gmail.com>
2026-09-14 17:19:34 -0600

    se trabajo con arreglos unidimensionales

---
+++ Arreglos.py
@@ -0,0 +1,32 @@
+#Declarando un arreglo
+numeros = [10,20,30,40,50]
+
+#Imprimiendo un elemento específico del arreglo
+print(numeros[2])
+
+#Reasignando un valor a un elemento específico del arreglo
+numeros[3] =35 
+print(numeros)
+
+#Agregando un elemento al final del arreglo
+numeros.append(60)
+print(numeros)
+
+#Eliminando un elemento específico del arreglo
+numeros.remove(35)
+print(numeros)
+
+#Eliminando un elemento en una posición específica del arreglo
+numeros.pop(4)
+print(numeros)
+
+#Declarando un arreglo de frutas
+Frutas = ["Manzana", "Fresa", "Sandia", "Mango", "Melon", "platano"]
+
+#Imprimiendo un elemento específico del arreglo
+Frutas.pop(4)
+print(Frutas)
+
+#Eliminando un elemento específico del arreglo
+Frutas.remove("Manzana")
+print(Frutas)
